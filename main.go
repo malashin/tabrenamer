fd9050eb9511dae2ad0131399f34d067df83bff2
malashin <malashinanton@gmail.com>
2019-08-29 15:08:58 +1000

    Use first argument as input, add more error checks

--- main.go
+++ main.go
@@ -6,36 +6,56 @@ import (
 	"io/ioutil"
 	"os"
 	"regexp"
+	"strings"
 )
 
 // Takes tab separated input file (src	dst) and renames found files in the folder.
 
-var inputPath = "input.txt"
 var re = regexp.MustCompile(`^(.+)\t(.+)$`)
 
 func main() {
+	if len(os.Args[1:]) < 1 {
+		fmt.Println("usage: tabrenamer [filelist]")
+		fmt.Println("tabrenamer takes tab separated input file (src\tdst) and renames found files in the folder.")
+		os.Exit(0)
+	}
+
+	inputPath := os.Args[1]
 	links, err := readLines(inputPath)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
+	}
+
+	if len(links) < 1 {
+		fmt.Println("ERROR: input file is empty")
+		return
 	}
 
 	files, err := ioutil.ReadDir("./")
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
 
 	filesMap := make(map[string]bool)
-
 	for _, f := range files {
 		filesMap[f.Name()] = true
 	}
 
 	for _, link := range links {
+		if strings.Count(link, "\t") > 1 {
+			fmt.Println("ERROR: input file contains more then one tab per line")
+			return
+		}
+
 		if !re.MatchString(link) {
 			fmt.Println("ERROR: input file contains wrong string pattern")
 			return
 		}
+	}
 
+	for _, link := range links {
 		in := re.ReplaceAllString(link, "${1}")
 		out := re.ReplaceAllString(link, "${2}")
 
@@ -43,7 +63,8 @@ func main() {
 			fmt.Println("RENAMING: " + in + " > " + out)
 			err = os.Rename(in, out)
 			if err != nil {
-				panic(err)
+				fmt.Println(err)
+				return
 			}
 		}
 	}
